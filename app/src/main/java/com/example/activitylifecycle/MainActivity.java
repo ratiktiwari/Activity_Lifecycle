531e457e5aa3c95fbc276fd50e199dec3f88ca17
Ratik <ratiktiwari@gmail.com>
2020-07-20 19:41:06 +0530

    Added all life Cycle Functions of an Activity

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "onCreate() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "onStart() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onStart()");
+    }
+
+    @Override
+    protected void onResume() {   // onPostResume() and onResume() is the same thing essentially
+        super.onResume();
+        Toast.makeText(MainActivity.this, "onResume() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "onPause() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "onStop() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "onDestroy() Called", Toast.LENGTH_SHORT).show();
+        Log.i("Cycle","onDestroy()");
+    }
+
+
+
 }
