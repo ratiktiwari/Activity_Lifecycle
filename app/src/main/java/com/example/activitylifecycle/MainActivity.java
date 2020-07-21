ec2e434aff392e46d787875b971f428c48910433
Ratik <ratiktiwari@gmail.com>
2020-07-21 22:07:11 +0530

    Input from editText and size of text in activity_show_guess.xml increased

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button showGuess;
+    private EditText enterGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         showGuess = findViewById(R.id.buttonGuess);
+        enterGuess = findViewById(R.id.editTextEnterGuess);
 
         showGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String guess = enterGuess.getText().toString().trim();
+
+                //Trim is used for below:
+                //Input :  str = "   Hello World   "
+                //Output : str = "Hello World"
+                //
+                //Input :  str = "      Hey  there    Joey!!!      "
+                //Output : str = "Hey  there    Joey!!!"
+
+
+                //We can eliminate the leading and trailing spaces of a string in Java with the help of trim().
+                //trim() method is defined under the String class of java.lang package.
+                //It does not eliminated the middle spaces of the string.
+                //By calling the trim() method, a new String object is returned.
+                //It doesn’t replace the value of String object. Therefore if we want the access to the new String object, we just need to reassign it to the old String or assign it to a new variable.
+
                 Intent intent = new Intent(MainActivity.this, ShowGuess.class);
 
-                intent.putExtra("guess", "Hello There");
+                intent.putExtra("guess", guess);
 
 //                startActivity(new Intent(MainActivity.this, ShowGuess.class));  //can do this way too
                 startActivity(intent);
