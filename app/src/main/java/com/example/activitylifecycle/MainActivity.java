fda39ab7a0ddb96751b07245c452275542b4b741
Ratik <ratiktiwari@gmail.com>
2020-07-21 20:01:22 +0530

    added Intent to the MainActivity.java

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -2,19 +2,34 @@ package com.example.activitylifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button showGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(MainActivity.this, "onCreate() Called", Toast.LENGTH_SHORT).show();
-        Log.i("Cycle","onCreate()");
+        showGuess = findViewById(R.id.buttonGuess);
+
+        showGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowGuess.class);
+                startActivity(intent);
+            }
+        });
+
+//        Toast.makeText(MainActivity.this, "onCreate() Called", Toast.LENGTH_SHORT).show();
+//        Log.i("Cycle","onCreate()");
     }
 
     @Override
